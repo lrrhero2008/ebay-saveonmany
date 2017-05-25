49417f8a620bbea30a095b00904d6cc0cf39da5b
alex <alex.l@shopperplus.com>
2017-05-25 15:07:23 +0800

    fix menu js

--- public/js/index.js
+++ public/js/index.js
@@ -1,93 +1,4 @@
 $(function() {
-	
-	// 幻灯片
-	(function() {
-		$(".main .paging").show();
-		$(".main .paging a:first").addClass("active");
-
-		var num = $('.main .paging a:first').attr('rel'); //右边导航幻灯片
-		var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-		var imgs = arr.slice((num - 1) * 3, num * 3);
-
-		for (var i = 0; i < imgs.length; i++) {
-			$('.' + imgs[i]).show();
-		}
-
-		var imageWidth = $(".main .window").width();
-		var imageSum = $(".main .image_reel img").size();
-		var imageReelWidth = imageWidth * imageSum;
-
-
-		$(".main .image_reel").css({
-			'width': imageReelWidth
-		});
-		// 第二部 动画开始
-
-		var rotate = function() {
-			var triggerID = $active.attr("rel") - 1;
-			var image_reelPosition = triggerID * imageWidth;
-
-
-			$(".main .paging a").removeClass('active');
-			$active.addClass('active');
-
-
-			$(".main .image_reel").animate({
-				left: -image_reelPosition
-			}, 500);
-
-			var act = $('.main .paging a.active').attr('rel');
-
-			var imgs = arr.slice((act - 1) * 3, act * 3);
-
-			for (var i = 0; i < imgs.length; i++) {
-				$('.' + imgs[i]).show();
-
-			}
-
-		};
-
-		var rotateSwitch = function() {
-			play = setInterval(function() {
-				$('.main .img').hide(); //所有人都隐藏
-				$active = $('.main .paging a.active').next();
-				if ($active.length === 0) {
-					$active = $('.main .paging a:first');
-				}
-				rotate();
-			}, 2000);
-		};
-		rotateSwitch();
-
-		// // 第三部移动和点击
-		$(".main .image_reel a").hover(function() {
-			clearInterval(play);
-
-		}, function() {
-			rotateSwitch();
-		});
-
-
-		$(".main .paging a").click(function() {
-			$active = $(this);
-			$('.img').hide(); //隐藏其他图片
-			var act = $(this).attr('rel');
-
-			var imgs = arr.slice((act - 1) * 3, act * 3);
-
-			for (var i = 0; i < imgs.length; i++) {
-				$('.' + imgs[i]).show();
-			}
-
-			clearInterval(play);
-			rotate();
-			rotateSwitch();
-			return false;
-
-		});
-	})();
-
-
 	//标签页
 	$('.indexTabBoxBottom .indexTabCon').first().show();
 
@@ -108,6 +19,10 @@ $(function() {
 		$('.indexTabRight .indexTabNewCon').eq(idx).show(); /*然后他们里面第几个让他们显示*/
 		$('.indexTabRight .indexTabNewCon').not($('.indexTabRight .indexTabNewCon').eq(idx)).hide(); /*其余的让他们隐藏*/
 	});
+	//菜单栏
+	$('ul.lev2').each(function(){
+		$(this).prev('li').append($(this))
+	})
 	//修改dom结构
 	$('body').addClass('saveonmany')
 	$('.div3').prepend($('.lev1'))
