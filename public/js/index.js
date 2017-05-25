19a8041f6e5f9aa7a902682440fa30774eb8d15b
alex <alex.l@shopperplus.com>
2017-05-25 15:51:55 +0800

    fix

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,4 @@
 $(function() {
-	//标签页
-	$('.indexTabBoxBottom .indexTabCon').first().show();
-
-	$('.indexTabBoxTop ul li').mouseenter(function() {
-		$(this).addClass('active'); /*鼠标移到哪个LI(标签)就添加个Active*/
-		$('.indexTabBoxTop ul li').not($(this)).removeClass('active'); /*其余的人移除*/
-		idx = $(this).index('.indexTabBoxTop ul li'); /*当前划住的li(标签页)在当前LI(标签页)排第几 */
-		$('.indexTabBoxBottom .indexTabCon').eq(idx).show(); /*然后他们里面第几个让他们显示*/
-		$('.indexTabBoxBottom .indexTabCon').not($('.indexTabBoxBottom .indexTabCon').eq(idx)).hide(); /*其余的让他们隐藏*/
-	});
-
-	$('.indexTabRight .indexTabNewCon').first().show();
-
-	$('.indexTabRight .indexTabNewNav ul li').mouseenter(function() {
-		$(this).addClass('active');
-		$('.indexTabRight .indexTabNewNav ul li').not($(this)).removeClass('active');
-		idx = $(this).index('.indexTabRight .indexTabNewNav ul li'); /*当前划住的li(标签页)在当前LI(标签页)排第几 */
-		$('.indexTabRight .indexTabNewCon').eq(idx).show(); /*然后他们里面第几个让他们显示*/
-		$('.indexTabRight .indexTabNewCon').not($('.indexTabRight .indexTabNewCon').eq(idx)).hide(); /*其余的让他们隐藏*/
-	});
-
 	//修改dom结构
 	$('body').addClass('saveonmany')
 	$('.div3').prepend($('.lev1'))
